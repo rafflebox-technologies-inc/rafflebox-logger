9ac18b64dbca9d1d5fb45780d45ccb8cecd81dd4
Jamie Archibald <jamie.archibald@rafflebox.ca>
2022-05-26 08:55:34 -0600

    Added rb client

--- src/extract-fields.ts
+++ src/extract-fields.ts
@@ -1,86 +1,86 @@
-import { format } from 'winston';
-import traverse from 'traverse';
+import { format } from 'winston'
+import traverse from 'traverse'
 
 // 1:1 mapping
-const captureKeys = ['email', 'province', 'client', 'locale', 'phone'];
+const captureKeys = ['email', 'province', 'client', 'rb-client', 'locale', 'phone']
 
 /**
  * For email, we grab the email from the first node/object.
  * And if we find another instance and it's already set then ignore it.
  */
-const extractFields = format((info) => {
-  const newFields: Record<string, any> = {};
+const extractFields = format(info => {
+  const newFields: Record<string, any> = {}
 
   const result = traverse(info).map(function extract() {
     if (this.key && captureKeys.includes(this.key)) {
       if (this.key === 'email' && newFields.email !== undefined) {
-        return;
+        return
       } else {
-        newFields[this.key] = this.node;
+        newFields[this.key] = this.node
       }
 
-      return;
+      return
     }
 
     if (this.key === 'serialNumber' || this.key === 'deviceSerialNumber') {
-      newFields.deviceSerialNumber = this.node;
+      newFields.deviceSerialNumber = this.node
 
-      return;
+      return
     }
 
     // province
     if (this.key === 'state' || this.key === 'provinceState') {
-      newFields.province = this.node;
+      newFields.province = this.node
 
-      return;
+      return
     }
 
     // event
     if (this.parent?.key === 'event') {
       if (this.key === 'id' && Number(this.node)) {
-        newFields.eventNumber = this.node;
+        newFields.eventNumber = this.node
       } else if ((this.key === 'id' || this.key === 'uuid') && typeof this.node === 'string') {
-        newFields.eventId = this.node;
+        newFields.eventId = this.node
       }
 
-      return;
+      return
     }
 
     // order
     if (this.parent?.key === 'order') {
       if (this.key === 'id' && Number(this.node)) {
-        newFields.orderNumber = this.node;
+        newFields.orderNumber = this.node
       } else if ((this.key === 'id' || this.key === 'uuid') && typeof this.node === 'string') {
-        newFields.orderId = this.node;
+        newFields.orderId = this.node
       } else if (this.key === 'referenceId') {
-        newFields.orderReferenceId = this.node;
+        newFields.orderReferenceId = this.node
       }
 
-      return;
+      return
     }
 
     // organization
     if (this.parent?.key === 'organization') {
       if (this.key === 'id' && Number(this.node)) {
-        newFields.organizationNumber = this.node;
+        newFields.organizationNumber = this.node
       } else if ((this.key === 'id' || this.key === 'uuid') && typeof this.node === 'string') {
-        newFields.organizationId = this.node;
+        newFields.organizationId = this.node
       }
     }
-  });
+  })
 
   // There is a bug in traverse.map where symbols are not copied to the new object
-  const levelSym = Symbol.for('level');
-  const splatSym = Symbol.for('splat');
+  const levelSym = Symbol.for('level')
+  const splatSym = Symbol.for('splat')
 
-  result[levelSym] = info[levelSym as unknown as string];
-  result[splatSym] = info[splatSym as unknown as string];
+  result[levelSym] = info[(levelSym as unknown) as string]
+  result[splatSym] = info[(splatSym as unknown) as string]
 
-  const data = { ...result[0], ...newFields };
+  const data = { ...result[0], ...newFields }
 
-  result[0] = data;
+  result[0] = data
 
-  return result;
-});
+  return result
+})
 
-export default extractFields;
+export default extractFields
